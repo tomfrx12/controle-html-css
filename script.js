58122392d79b0cb0fe4074db8d9353b6f529e62d
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2024-12-26 22:00:41 +0100

    RENDU

--- script.js
+++ script.js
@@ -2,31 +2,38 @@ function toggleDarkMode() {
     const DarkMode = document.querySelector("#modeSombre");
     const body = document.querySelector("body");
 
-    if (DarkMode) {
-        function modeSombre() {
-            body.classList.toggle("sombre");
+    function modeSombre() {
+        body.classList.toggle("sombre");
 
-            if (body.classList.contains("sombre")) {
-                DarkMode.textContent = "☀";
-                document.documentElement.style.setProperty('--color-black', '#d9cdc7');
-                document.documentElement.style.setProperty('--color-neutral', '#0a0805');
-                document.documentElement.style.setProperty('--color-grey', '#8e8e8e');
-                document.documentElement.style.setProperty('--color-d-grey', '#b2b2b2');
-                document.documentElement.style.setProperty('--color-white', '#000000');
-
-            } else {
-                DarkMode.textContent = "🌙";
-                document.documentElement.style.setProperty('--color-black', '#263238');
-                document.documentElement.style.setProperty('--color-neutral', '#F5F7FA');
-                document.documentElement.style.setProperty('--color-grey', '#717171');
-                document.documentElement.style.setProperty('--color-d-grey', '#4D4D4D');
-                document.documentElement.style.setProperty('--color-white', '#ffffff');
-            }
+        if (body.classList.contains("sombre")) {
+            DarkMode.textContent = "☀";
+            document.documentElement.style.setProperty('--color-black', '#d9cdc7');
+            document.documentElement.style.setProperty('--color-neutral', '#0a0805');
+            document.documentElement.style.setProperty('--color-grey', '#8e8e8e');
+            document.documentElement.style.setProperty('--color-d-grey', '#b2b2b2');
+            document.documentElement.style.setProperty('--color-white', '#000000');
+        } else {
+            DarkMode.textContent = "🌙";
+            document.documentElement.style.setProperty('--color-black', '#263238');
+            document.documentElement.style.setProperty('--color-neutral', '#F5F7FA');
+            document.documentElement.style.setProperty('--color-grey', '#717171');
+            document.documentElement.style.setProperty('--color-d-grey', '#4D4D4D');
+            document.documentElement.style.setProperty('--color-white', '#ffffff');
         }
-        DarkMode.addEventListener("click", modeSombre);
     }
+
+    DarkMode.addEventListener("click", modeSombre);
+}
+
+function showSignUpAlert() {
+    const ButtonLogin = document.getElementById('login');
+    
+    ButtonLogin.addEventListener('click', function() {
+        alert("Bienvenue sur la page d'inscription !");
+    });
 }
 
 window.addEventListener("DOMContentLoaded", function () {
     toggleDarkMode();
+    showSignUpAlert();
 });
